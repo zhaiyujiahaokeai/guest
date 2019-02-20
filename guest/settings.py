f3c7a6a9557f0d06ce4ab0b9de6cb57414424fc6
zhaiyujiahaokeai <1907049504@qq.com>
2019-02-20 09:27:13 +0800

    进一步完成数据库表设计

--- guest/settings.py
+++ guest/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'guest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3308',
+        'NAME': 'guest',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
@@ -112,7 +119,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
--- sign/admin.py
+++ sign/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from sign.models import Event, Guest
+
 
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'status', 'address', 'start_time']
+    search_fields = ['name']   # 搜索栏
+    list_filter = ['status']    # 过滤栏
+
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['realname', 'phone', 'email', 'sign', 'create_time', 'event']
+    search_fields = ['realname', 'phone']   # 搜索栏
+    list_filter = ['sign']           # 过滤栏
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Guest, GuestAdmin)
