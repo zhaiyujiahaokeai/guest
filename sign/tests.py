600fcef7377a82cb5479c42859f00bc521b9b798
zhaiyujiahaokeai <1907049504@qq.com>
2019-02-27 15:04:44 +0800

    添加针对发布会管理的测试用例

--- sign/tests.py
+++ sign/tests.py
@@ -63,3 +63,27 @@ class LoginActionTest(TestCase):
         test_data = {'username': 'admin', 'password': 'admin123456'}
         response = self.client.post('/login_action/', data=test_data)
         self.assertEqual(response.status_code, 302)
+
+
+class EventMangeTest(TestCase):
+    # 发布会管理
+    def setUp(self):
+        User.objects.create_user('admin', 'admin@mail.com', 'admin123456')
+        Event.objects.create(name="xiaomi5", limit=2000, address='beijing', status=1, start_time='2017-8-10 12:30:00')
+        self.login_user = {'username': 'admin', 'password': 'admin123456'}
+
+    def test_event_mange_success(self):
+        # 测试发布会:xiaomi5
+        # response = self.client.post('/login_action/', data=self.login_user)
+        response = self.client.post('/event_manage/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"xiaomi5", response.content)
+        self.assertIn(b"beijing", response.content)
+
+    def test_event_mange_search_success(self):
+        # 测试发布会搜索
+        # response = self.client.post('/login_action/', data=self.login_user)
+        response = self.client.post('/search_name/', {"name": "xiaomi5"})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"xiaomi5", response.content)
+        self.assertIn(b"beijing", response.content)
